ebd96828d3dc60854e0ca8d7867f058838b2da4b
wengers1004 <wengers1004@gmail.com>
2019-10-02 12:58:32 +0900

    prototype 정의 중

--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import {observable, action} from 'mobx'
+import {observer} from 'mobx-react'
 import AddCircleIcon from '@material-ui/icons/AddCircle';
-export default class Reviews extends Component {
+@observer
+class Reviews extends Component {
+    @observable reviews = [];
+    @observable switch = '';
     @action
     addReview = () => {
-
+        console.log("object")
+        if(this.switch===''){
+            console.log('in')
+            this.switch = <div>title and content input</div>
+        }else{
+            this.switch = ''
+        }
     }
     render() {
         return (
             <div>
-                
+                {this.reviews}
+                {this.switch}
                 <AddCircleIcon onClick = {this.addReview}/>
             </div>
         )
     }
 }
+export default Reviews
